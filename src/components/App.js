7d30dbd979136a0fb4c9ae5625f467fefefd3d81
Aleh Atsman <aleh.atsman@gmail.com>
2019-03-25 13:38:45 +0100

    Add google analytics

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import App, { Container } from 'next/app'
 import React from 'react'
 import { GlobalStyle } from 'styles/global'
+import Router from 'next/router'
+import withGA from 'next-ga'
 
 class MyApp extends App {
   static async getInitialProps ({ Component, ctx }) {
@@ -24,4 +26,4 @@ class MyApp extends App {
   }
 }
 
-export default MyApp
+export default withGA('UA-99002751-2', Router)(MyApp)
