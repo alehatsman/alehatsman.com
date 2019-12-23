9399db5329b9749d059047e6dd05f81e6b46ed05
Aleh Atsman <aleh.atsman@gmail.com>
2019-12-23 15:31:41 +0100

    Add mixpanel

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import App from 'next/app'
 import React from 'react'
 import Router from 'next/router'
 import withGA from 'next-ga'
+import mixpanel from 'mixpanel-browser'
 
 import { GlobalStyle } from '@/styles/global'
 
@@ -30,6 +31,7 @@ class MyApp extends App {
 let myApp
 if (process.env.NODE_ENV !== 'development') {
   myApp = withGA(process.env.GA_ID, Router)(MyApp)
+  mixpanel.init('5538d2616018534e9694d0a8bcd1a6f3')
 } else {
   myApp = MyApp
 }
