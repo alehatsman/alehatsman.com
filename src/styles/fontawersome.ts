d4f461e2a21a1cbd708ef6337a1db41b661f62c0
Aleh Atsman <aleh.atsman@gmail.com>
2020-01-03 15:47:44 +0100

    Remove unused icons

--- src/styles/fontawersome.ts
+++ src/styles/fontawersome.ts
@@ -1,4 +1,3 @@
-import { faAt, faLocationArrow } from '@fortawesome/free-solid-svg-icons'
 import { faGithubAlt, faTelegram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
 
@@ -7,6 +6,4 @@ library.add(
   faGithubAlt,
   faTelegram,
   faTwitter,
-  faAt,
-  faLocationArrow
 )
